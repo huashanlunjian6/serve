a476b87c1e2b3f8b77533217f46969caf42c9413
riki_yue <490629543@qq.com>
2020-10-16 21:26:46 +0800

    提交订单详情接口,修改订单列表接口代码

--- app.js
+++ app.js
@@ -36,24 +36,34 @@ server.listen(3000,()=>{
   console.log('server is running...');
 });
 
-
+//订单列表接口by张玥
 server.post('/orderList',(req,res)=>{
   let role=parseInt(req.body.role);
-  let uid=req.body.uid;
-  let page=req.body.page;
+  let uid=parseInt(req.body.uid);
+  let page=parseInt(req.body.page);
   let pageSize=10;
   let offset=(page-1)*pageSize;
   if(role == 0){
-    let sql='SELECT r_title, old_town, r_room, r_bed, r_people, order_time,all_price FROM gz_order go LEFT JOIN gz_home_resources ghr ON o_rid=rid LEFT JOIN gz_old_town got ON got.`tid`=ghr.`r_tid` WHERE go.`r_uid`=? LIMIT '+ offset + ',' + pageSize;
+    let sql='SELECT oid,r_title, old_town, r_room, r_bed, r_people, order_time,all_price FROM gz_order go LEFT JOIN gz_home_resources ghr ON o_rid=rid LEFT JOIN gz_old_town got ON got.`tid`=ghr.`r_tid` WHERE go.`r_uid`=? LIMIT '+ offset + ',' + pageSize;
     pool.query(sql,[uid],(error,results)=>{
       if(error) throw error;
       res.send({message:'查询成功',code:1,results});
     });
   }else{
-    let sql='SELECT r_title, old_town, r_room, r_bed, r_people, order_time,all_price FROM gz_order go LEFT JOIN gz_home_resources ghr ON o_rid=rid LEFT JOIN gz_old_town got ON got.`tid`=ghr.`r_tid` WHERE ghr.`r_uid`=? LIMIT '+ offset + ',' + pageSize;
+    let sql='SELECT oid,r_title, old_town, r_room, r_bed, r_people, order_time,all_price FROM gz_order go LEFT JOIN gz_home_resources ghr ON o_rid=rid LEFT JOIN gz_old_town got ON got.`tid`=ghr.`r_tid` WHERE ghr.`r_uid`=? LIMIT '+ offset + ',' + pageSize;
     pool.query(sql,[uid],(error,results)=>{
       if(error) throw error;
       res.send({message:'查询成功',code:1,results});
     });
   }
+});
+
+//订单详情接口by张玥
+server.post('/orderDetails',(req,res)=>{
+  let oid=parseInt(req.body.oid);
+  let sql='SELECT r_title, old_town, enter_time, leave_time, r_room, r_people,all_price,r_margin FROM gz_order go LEFT JOIN gz_home_resources ghr ON o_rid=rid LEFT JOIN gz_old_town got ON got.`tid`=ghr.`r_tid` WHERE go.`oid`=?';
+  pool.query(sql,[oid],(error,results)=>{
+    if(error) throw error;
+    res.send({message:'查询成功',code:1,results});
+  });
 });
