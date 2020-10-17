975ec0f0972bc5ff1673509c86d6c16271e7b45d
riki_yue <490629543@qq.com>
2020-10-17 22:15:18 +0800

    订单列表及订单详情接口修改,完善分页功能;

--- app.js
+++ app.js
@@ -41,26 +41,44 @@ server.post('/orderList',(req,res)=>{
   let role=parseInt(req.body.role);
   let uid=parseInt(req.body.uid);
   let page=parseInt(req.body.page);
-  let pageSize=10;
+  let pageSize=5;
   let offset=(page-1)*pageSize;
   if(role == 0){
     let sql='SELECT oid,r_title, old_town, r_room, r_bed, r_people, order_time,all_price FROM gz_order go LEFT JOIN gz_home_resources ghr ON o_rid=rid LEFT JOIN gz_old_town got ON got.`tid`=ghr.`r_tid` WHERE go.`r_uid`=? LIMIT '+ offset + ',' + pageSize;
     pool.query(sql,[uid],(error,results)=>{
       if(error) throw error;
-      res.send({message:'查询成功',code:1,results});
+      ////////////////////////
+      let sql='SELECT COUNT(oid) AS count FROM gz_order WHERE r_uid = ?';
+      let rowcount;
+      let pagecount;
+      pool.query(sql,[uid],(err,result)=>{
+        if(err) throw err;
+        rowcount=result[0].count;
+        pagecount=Math.ceil(rowcount/pageSize);
+        res.send({message:'查询成功',code:1,results,pagecount});
+      })
     });
   }else{
     let sql='SELECT oid,r_title, old_town, r_room, r_bed, r_people, order_time,all_price FROM gz_order go LEFT JOIN gz_home_resources ghr ON o_rid=rid LEFT JOIN gz_old_town got ON got.`tid`=ghr.`r_tid` WHERE ghr.`r_uid`=? LIMIT '+ offset + ',' + pageSize;
     pool.query(sql,[uid],(error,results)=>{
       if(error) throw error;
-      res.send({message:'查询成功',code:1,results});
+      ////////////////////////
+      let sql='SELECT COUNT(oid) AS count FROM gz_order go LEFT JOIN gz_home_resources ghr ON o_rid=rid LEFT JOIN gz_old_town got ON got.`tid`=ghr.`r_tid` WHERE ghr.`r_uid`=?';
+      let rowcount;
+      let pagecount;
+      pool.query(sql,[uid],(err,result)=>{
+        if(err) throw err;
+        rowcount=result[0].count;
+        pagecount=Math.ceil(rowcount/pageSize);
+        res.send({message:'查询成功',code:1,results,pagecount});
+      })
     });
   }
 });
 
 //订单详情接口by张玥
-server.post('/orderDetails',(req,res)=>{
-  let oid=parseInt(req.body.oid);
+server.get('/orderDetails',(req,res)=>{
+  let oid=parseInt(req.query.oid);
   let sql='SELECT r_title, old_town, enter_time, leave_time, r_room, r_people, o_enter_person_name, o_enter_person_phone, o_enter_person_idcard, all_price, r_margin FROM gz_order go LEFT JOIN gz_home_resources ghr ON o_rid=rid LEFT JOIN gz_old_town got ON got.`tid`=ghr.`r_tid` WHERE go.`oid`=?';
   pool.query(sql,[oid],(error,results)=>{
     if(error) throw error;
